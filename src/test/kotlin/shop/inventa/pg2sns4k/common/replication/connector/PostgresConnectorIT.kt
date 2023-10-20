3f4fbbd45ca9e81685d36057f0b5a9ce22d70c72
Fernando Fernandes <fernando.junior@inventa.shop>
2023-10-20 16:20:55 -0300

    Improve PostgresConnector test

--- src/test/kotlin/shop/inventa/pg2sns4k/common/replication/connector/PostgresConnectorIT.kt
+++ src/test/kotlin/shop/inventa/pg2sns4k/common/replication/connector/PostgresConnectorIT.kt
@@ -20,6 +20,7 @@ import java.sql.Connection
 import java.util.Properties
 import java.util.function.Consumer
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 import kotlin.test.assertNull
 
 @Testcontainers
@@ -67,14 +68,17 @@ internal class PostgresConnectorIT {
         val emitMessageCommand = "SELECT pg_logical_emit_message(true, 'catalogue-collection-business-events', '{\"headers\":{\"eventType\":\"eventType\",\"eventTimestamp\":\"2023-10-18T15:37:47.539787\"},\"body\":{\"eventUUID\":\"2dcffe9d-d191-4155-ab11-4a3b4125f3a9\",\"eventType\":\"eventType\",\"domainId\":\"domainId\",\"domain\":\"domain\",\"eventTimestamp\":\"2023-10-18T15:37:47.539787\",\"payload\":{\"uuid\":\"4ee8b1fb-f002-4d52-b0bc-ec840786f3cb\",\"shopifyCollectionId\":null,\"name\":\"Collection Cool\",\"slug\":\"collection-cool\",\"description\":\"Collection Cool Description\",\"status\":\"status\",\"disjunctive\":false,\"createdAt\":\"2023-10-18T15:37:47.540245\",\"updatedAt\":\"2023-10-18T15:37:47.540251\"}}}')"
 
         // when
+        val beforeLSN = postgresConnector.currentLSN()
         val beforeCommandResultBytes = postgresConnector.readPending()
         val result = executeCommand(emitMessageCommand)
         val afterCommandResultBytes = postgresConnector.readPending()
+        val afterLSN = postgresConnector.currentLSN()
 
         // then
         assertEquals(true, result)
         assertNull(beforeCommandResultBytes)
         assertNotNull(afterCommandResultBytes)
+        assertNotEquals(beforeLSN, afterLSN)
     }
 
     private fun configureContainer() {
