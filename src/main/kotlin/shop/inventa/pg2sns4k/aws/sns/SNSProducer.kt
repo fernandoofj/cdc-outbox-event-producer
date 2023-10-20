5656f1a67eb53241900f06c630dce6e6b05f5c5c
Fernando Fernandes <fernando.junior@inventa.shop>
2023-10-20 17:58:09 -0300

    Fix class

--- src/main/kotlin/shop/inventa/pg2sns4k/aws/sns/SNSProducer.kt
+++ src/main/kotlin/shop/inventa/pg2sns4k/aws/sns/SNSProducer.kt
@@ -4,5 +4,5 @@ import shop.inventa.pg2sns4k.aws.sns.dto.SNSMessage
 
 interface SNSProducer {
 
-    fun <T : Any> send(topicName: String, message: shop.inventa.pg2sns4k.aws.sns.dto.SNSMessage<T>)
+    fun <T : Any> send(topicName: String, message: SNSMessage<T>)
 }
